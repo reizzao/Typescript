af28c8e1d31809fd0faa3d4a0ad72e135b60c0ec
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-06-07 15:02:02 -0300

    ok

--- API_Base_POO/src/externals/libs.ts
+++ API_Base_POO/src/externals/libs.ts
@@ -1,11 +1,11 @@
 // @externals
 
-export { Generator as Generator } from '/home/rzj/x/_github_rz_/SuperLibs_TS/SuperLibsTS/Generators/lib.ts'
+export { Generator as Generator } from '/home/rzj/x/_github_rz_/_NO_GITHUB_/RzLibs/RzLibs_TS/Generators/lib.ts'
 
-export { log as Log } from '/home/rzj/x/_github_rz_/SuperLibs_TS/SuperLibsTS/LogConsole/lib.ts'
+export { log as Log } from '/home/rzj/x/_github_rz_/_NO_GITHUB_/RzLibs/RzLibs_TS/LogConsole/lib.ts'
 
-export { pipeParamRules as pipeParamRules } from "/home/rzj/x/_github_rz_/SuperLibs_TS/SuperLibsTS/pipesFN/pipeAsync_FNsParamObject_Rules/lib.ts"
+export { pipeParamRules as pipeParamRules } from "/home/rzj/x/_github_rz_/_NO_GITHUB_/RzLibs/RzLibs_TS/pipesFN/pipeAsync_FNsParamObject_Rules/lib.ts"
 
-export { minLength as minLength } from "/home/rzj/x/_github_rz_/SuperLibs_TS/SuperLibsTS/Validators/minLength/decorator/lib.ts"
+export { minLength as minLength } from "/home/rzj/x/_github_rz_/_NO_GITHUB_/RzLibs/RzLibs_TS/Validators/minLength/decorator/lib.ts"
 
-export { type IMyRulesIS as IMyRulesIS, type IMyRulesISFN as IMyRulesISFN } from "/home/rzj/x/_github_rz_/SuperLibs_TS/SuperLibsTS/Rules/lib.ts"
+export { type IMyRulesIS as IMyRulesIS, type IMyRulesISFN as IMyRulesISFN } from "/home/rzj/x/_github_rz_/_NO_GITHUB_/RzLibs/RzLibs_TS/Rules/lib.ts"
