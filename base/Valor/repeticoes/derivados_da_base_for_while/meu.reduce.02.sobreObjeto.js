70eacffd2de501858db7c6a94c91f8021e2e20a0
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-07-30 14:58:21 -0300

    todo: meuReduce sobre objeto

--- base/Valor/repeticoes/derivados_da_base_for_while/meu.reduce.02.sobreObjeto.js
+++ base/Valor/repeticoes/derivados_da_base_for_while/meu.reduce.02.sobreObjeto.js
@@ -1,7 +1,7 @@
 // REDUCE SOBRE OBJETO
 
-let dadoInicial = ""
-let dadoAlvo = "A"
+let dadoInicial = { c1: "A", c2: "B"}
+let dadoAlvo = { c1: "A", c2: "B"}
 
 const func1 = function () { return dadoAlvo }()
 const func2 = function () { return dadoAlvo }()
@@ -11,13 +11,15 @@ const arrayAlvo = [ func1, func2, func3 ]
 
 // by meuReduce
 function acumulador (initial, item) {
-  return initial = item
+  return initial += item
   // += é a operacao que quero fazer entre os items para acumula-los e ter um resultado final
 }
 
 function opSobreAcumulados (itemAcumulado) {
-	return itemAcumulado += "----OOOOO-----"
+  // let dadoAlvo = dadoAlvo
+	return itemAcumulado.c1 += "----OOOOO-----"
 }
+// TODO MUDAR SOMENTE A CHAVE c1 do dado alvo
 
 // meuReduce para uso
 const useMeuReduce = (array, initial) => {
