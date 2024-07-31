0dad17d94009cea547078af6506c79b14418e660
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-07-31 11:50:46 -0300

    feat: reduce js - ok sem undefined em textos

--- base/Valor/repeticoes/derivados_da_base_for_while/myREDUCE/js/lib/lib.js
+++ base/Valor/repeticoes/derivados_da_base_for_while/myREDUCE/js/lib/lib.js
@@ -6,10 +6,11 @@ function auxAccumulator(initial, item) {
 // myReduce para uso
 export const useREDUCE = (list, opAccumulators, opFinal, initial) => {
   try {
+    const initContador = 0
     const arr = list;
     let operacao = initial;
 
-    for (let i = initial; i < arr.length; i++) {
+    for (let i = initContador; i < arr.length; i++) {
       operacao = auxAccumulator(operacao, opAccumulators(arr[i]));
     }
     const opFinalaposAcumulados = opFinal(operacao);
--- base/Valor/repeticoes/derivados_da_base_for_while/myREDUCE/js/use_Lib/reduce.01.NUMEROS.js
+++ base/Valor/repeticoes/derivados_da_base_for_while/myREDUCE/js/use_Lib/reduce.01.NUMEROS.js
@@ -1,4 +1,4 @@
-import { useREDUCE } from "../lib/lib.js";
+import { useREDUCE } from "../js/../lib/lib.js";
 
 const dadoOrigem = { c1: 10, c2: 10 };
 let dadoAlvo = dadoOrigem.c1;
@@ -20,12 +20,12 @@ const listData = [func1, func2, func3];
 
 // Dinamico é a operacao em cada dadoAlvo para acumular
 function opSobreCadaAcumulados(itemAcumulado) {
-  return itemAcumulado * 3;
+  return itemAcumulado * 2;
 }
 
 // Dinamico é a operacao final no resultado da acumulacao final
 function opFinalNoTotal(itemAcumulado) {
-  return itemAcumulado + 2;
+  return itemAcumulado + 1;
 }
 
 const resultREDUCE = useREDUCE(
