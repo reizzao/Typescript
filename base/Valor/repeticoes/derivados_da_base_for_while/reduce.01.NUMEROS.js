fbfc91770e0d833c7e560a1ce2f102efbf21e8ba
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-07-31 11:16:33 -0300

    ok

--- base/Valor/repeticoes/derivados_da_base_for_while/reduce.01.NUMEROS.js
+++ base/Valor/repeticoes/derivados_da_base_for_while/reduce.01.NUMEROS.js
@@ -1,4 +1,3 @@
-
 const dadoOrigem = { c1: 10, c2: 10 };
 let dadoAlvo = dadoOrigem.c1;
 let inicial = 0;
@@ -15,7 +14,7 @@ const func3 = function () {
   return dadoAlvo;
 }();
 
-const arrayAlvo = [func1, func2, func3];
+const listData = [func1, func2, func3];
 
 // by meuReduce : acumula em unica var os dados
 function acumulador(initial, item) {
@@ -24,7 +23,7 @@ function acumulador(initial, item) {
 
 // Dinamico é a operacao em cada dadoAlvo para acumular
 function opSobreCadaAcumulados(itemAcumulado) {
-  return itemAcumulado + 10;
+  return itemAcumulado * 3;
 }
 
 // Dinamico é a operacao final no resultado da acumulacao final
@@ -33,12 +32,12 @@ function opFinalNoTotal(itemAcumulado) {
 }
 
 // meuReduce para uso
-const useMeuReduce = (array, opAcumulados, opFinal, initial) => {
-  const arr = array;
+const useMeuReduce = (list, opAccumulators, opFinal, initial) => {
+  const arr = list;
   let operacao = initial;
 
   for (let i = initial; i < arr.length; i++) {
-    operacao = acumulador(operacao, opAcumulados(arr[i]));
+    operacao = acumulador(operacao, opAccumulators(arr[i]));
   }
   const opFinalaposAcumulados = opFinal(operacao);
 
@@ -46,13 +45,14 @@ const useMeuReduce = (array, opAcumulados, opFinal, initial) => {
 };
 
 const resultREDUCE = useMeuReduce(
-  arrayAlvo,
+  listData,
   opSobreCadaAcumulados,
   opFinalNoTotal,
   inicial,
 );
 
 console.log(resultREDUCE);
+// console.log(dadoOrigem); // Obs: dado de origem nao é alterado
 
 /*
 
